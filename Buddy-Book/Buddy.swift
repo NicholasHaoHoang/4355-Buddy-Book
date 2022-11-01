ea5877d2de13308a9d9940ef20353dbdbaee63fc
NicholasHaoHoang <haohoang2000@gmail.com>
2022-10-31 19:52:19 -0500

    favorite button

--- Buddy-Book/Buddy.swift
+++ Buddy-Book/Buddy.swift
@@ -10,23 +10,26 @@ class Buddy{
     //parameters
     var name = ""
     var profilePicture: UIImage
+    var isFav : Bool
     
     //default constructor
     init(){
         self.name = ""
         self.profilePicture = UIImage()
+        self.isFav = false
     }
     
     //constructor with parameters
-    init(name: String, profilePicture: UIImage){
+    init(name: String, profilePicture: UIImage, isFav:Bool=false){
         self.name = name
         self.profilePicture = profilePicture
+        self.isFav = isFav
     }
     
     //Dummy Data
     static func fetchBuddys() -> [Buddy]{
         return [
-            Buddy(name: "Bob", profilePicture: UIImage(named: "bobProfilePic")!),
+            Buddy(name: "Bob", profilePicture: UIImage(named: "bobProfilePic")!, isFav: true),
             Buddy(name: "Steven", profilePicture: UIImage(named: "stevenProfilePic")!),
             Buddy(name: "JoAnn", profilePicture: UIImage(named: "joannProfilePic")!),
         ]
