eb0a6b2b5a219c68013538b79ecf6753e29ce5c2
NicholasHaoHoang <haohoang2000@gmail.com>
2022-10-31 22:20:45 -0500

    Updated Buddy Class

--- Buddy-Book/Buddy.swift
+++ Buddy-Book/Buddy.swift
@@ -11,19 +11,52 @@ class Buddy{
     var name = ""
     var profilePicture: UIImage
     var isFav : Bool
+    var birthday: Date
+    var height : String
+    var weight : String
+    var likes : [String]
+    var dislikes : [String]
+    var giftIdeas : [String]
+    var bio : String
+    var importantFacts : [String]
+    var importantDates : [Date]
+    var relationships : [Buddy]
+    var gallery : [UIImage]
     
     //default constructor
     init(){
         self.name = ""
         self.profilePicture = UIImage()
         self.isFav = false
+        self.birthday = Date()
+        self.height = ""
+        self.weight = ""
+        self.likes = [""]
+        self.dislikes = [""]
+        self.giftIdeas = [""]
+        self.bio = ""
+        self.importantFacts = [""]
+        self.importantDates = [Date()]
+        self.relationships = [Buddy()]
+        self.gallery = [UIImage()]
     }
     
     //constructor with parameters
-    init(name: String, profilePicture: UIImage, isFav:Bool=false){
+    init(name: String, profilePicture: UIImage, isFav:Bool=false, birthday: Date = Date(), height : String = "", weight : String = "", likes : [String] = [""], dislikes : [String] = [""], giftIdeas : [String] = [""], bio : String = "", importantFacts : [String] = [""], importantDates : [Date] = [Date()], relationships : [Buddy] = [Buddy()], gallery : [UIImage] = [UIImage()]){
         self.name = name
         self.profilePicture = profilePicture
-        self.isFav = isFav 
+        self.isFav = isFav
+        self.birthday = birthday
+        self.height = height
+        self.weight = weight
+        self.likes = likes
+        self.dislikes = dislikes
+        self.giftIdeas = giftIdeas
+        self.bio = bio
+        self.importantFacts = importantFacts
+        self.importantDates = importantDates
+        self.relationships = relationships
+        self.gallery = gallery
     }
     
     //Dummy Data
