04cf0e74dba852366caf5096ecdb13d99ade0aa1
Hugh Hoang <46976769+HughHoang@users.noreply.github.com>
2022-11-28 06:23:09 -0600

    Add files via upload

--- Buddy-Book/Buddy.swift
+++ Buddy-Book/Buddy.swift
@@ -5,7 +5,13 @@
 
 import Foundation
 import UIKit
-
+extension Date {
+    func string(format: String) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = format
+        return formatter.string(from: self)
+    }
+}
 class Buddy{
     //parameters
     var name = ""
@@ -19,9 +25,8 @@ class Buddy{
     var giftIdeas : [String]
     var bio : String
     var importantFacts : [String]
-    var importantDates : [Date]
-    var relationships : [Buddy]
-    var gallery : [UIImage]
+    var importantDates : [String]
+    var relationships : [String]
     
     //default constructor
     init(){
@@ -36,13 +41,12 @@ class Buddy{
         self.giftIdeas = [""]
         self.bio = ""
         self.importantFacts = [""]
-        self.importantDates = [Date()]
-        self.relationships = [Buddy()]
-        self.gallery = [UIImage()]
+        self.importantDates = [String()]
+        self.relationships = [String()]
     }
     
     //constructor with parameters
-    init(name: String, profilePicture: UIImage, isFav:Bool=false, birthday: Date = Date(), height : String = "", weight : String = "", likes : [String] = [""], dislikes : [String] = [""], giftIdeas : [String] = [""], bio : String = "", importantFacts : [String] = [""], importantDates : [Date] = [Date()], relationships : [Buddy] = [Buddy()], gallery : [UIImage] = [UIImage()]){
+    init(name: String, profilePicture: UIImage, isFav:Bool=false, birthday: Date = Date(), height : String = "", weight : String = "", likes : [String] = [""], dislikes : [String] = [""], giftIdeas : [String] = [""], bio : String = "", importantFacts : [String] = [""], importantDates : [String] = [String()], relationships : [String] = [String()]){
         self.name = name
         self.profilePicture = profilePicture
         self.isFav = isFav
@@ -56,15 +60,44 @@ class Buddy{
         self.importantFacts = importantFacts
         self.importantDates = importantDates
         self.relationships = relationships
-        self.gallery = gallery
     }
-    
+   
     //Dummy Data
     static func fetchBuddys() -> [Buddy]{
         return [
-            Buddy(name: "Bob", profilePicture: UIImage(named: "bobProfilePic")!, isFav: true),
-            Buddy(name: "Steven", profilePicture: UIImage(named: "stevenProfilePic")!),
-            Buddy(name: "JoAnn", profilePicture: UIImage(named: "joannProfilePic")!),
+            Buddy(name: "Bob", profilePicture: UIImage(named: "bobProfilePic")!, isFav: true,
+                  birthday: Date(timeIntervalSinceReferenceDate: -123456789.0),
+                  height : "5' 9",
+                  weight : "150",
+                  likes : ["Gaming","Eating"],
+                  dislikes : ["Parties","Country Music"],
+                  giftIdeas : ["Plushy","Keychain"],
+                  bio : "Brown-Haired guy, the one with glasses",
+                  importantFacts : ["Met him at twitchcon, works on fridays"],
+                  importantDates : ["10/12 go to beach"],
+                  relationships : ["Steven"]),
+            Buddy(name: "Steven", profilePicture: UIImage(named: "stevenProfilePic")!, isFav: true,
+                  birthday: Date(timeIntervalSinceReferenceDate: -123456789.0),
+                  height : "5' 9",
+                  weight : "150",
+                  likes : ["Gaming","Eating"],
+                  dislikes : ["Parties","Country Music"],
+                  giftIdeas : ["Plushy","Keychain"],
+                  bio : "Brown-Haired guy, the one with glasses",
+                  importantFacts : ["Met him at twitchcon, works on fridays"],
+                  importantDates : ["10/12 go to beach"],
+                  relationships : ["Steven"]),
+            Buddy(name: "JoAnn", profilePicture: UIImage(named: "joannProfilePic")!, isFav: true,
+                  birthday: Date(timeIntervalSinceReferenceDate: -123456789.0),
+                  height : "5' 9",
+                  weight : "150",
+                  likes : ["Gaming","Eating"],
+                  dislikes : ["Parties","Country Music"],
+                  giftIdeas : ["Plushy","Keychain"],
+                  bio : "Brown-Haired guy, the one with glasses",
+                  importantFacts : ["Met him at twitchcon, works on fridays"],
+                  importantDates : ["10/12 go to beach"],
+                  relationships : ["Steven"]),
         ]
     }
 }
